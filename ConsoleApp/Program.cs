1aa23453aca5efe16a2c0b448dc21292b3a991d7
CSDotNET0211 <me@csdotnet.dev>
2024-03-10 16:11:49 +0900

    minor fix

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,10 +9,6 @@ START: ;
 string jsonString = string.Empty;
 //string fileName = Console.ReadLine();
 string fileName = @"C:\Users\CSDotNET\Downloads\config.ttc";
-fileName = fileName.Replace("\"", 
-var json=JsonSerializer.Deserialize<ConfigDataTTC>(File.ReadAllText(fileName));
-"");
-
 using (StreamReader reader = new StreamReader(fileName, Encoding.UTF8))
 {
 	jsonString = reader.ReadToEnd();
